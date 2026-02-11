976f0f91217dfde778860d81ffde59e2104b026d
Naveen <naveenxd@devh.in>
2026-02-11 19:07:29 +0530

    change app icon

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,21 +22,25 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "in.devh.voidx"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("/home/naveenxd/JKSKEYS/my-release-key.jks") // Change 'user' to your actual username
+            storePassword = "devhxd"
+            keyAlias = "devhxd"
+            keyPassword = "devhxd"
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
